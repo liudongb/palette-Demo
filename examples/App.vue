2ec7f55491a433856d2773f916ab7fc3e974541b
liudongbing <2429219310@qq.com>
2021-01-12 14:29:27 +0800

    画板项目demo

---
+++ examples/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <palette></palette>
+  </div>
+</template>
+
+<script>
+import palette from '../packages/palette/index';
+export default {
+  name: 'app',
+  components: {
+    palette
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  margin: 0px;
+  width: 100%;
+  height: 100vh;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  margin: 0px;
+  width: 100%;
+  height: 100vh;
+}
+</style>
---
+++ packages/palette/index.js
@@ -0,0 +1,4 @@
+import preventScroll from './util/preventScroll.js';
+preventScroll();
+import Palette from './src/palette.vue';
+export default Palette;
